1019f502f60d622e040a3ae8a1a8de0572cbeec0
richimenvic <93284693+richimenvic@users.noreply.github.com>
2026-03-05 13:44:40 +0100

    Delete instalar_pyMenvic.bat

--- instalar_pyMenvic.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Instalando pyMenvic desde GitHub...
-pyrevit extend "pyMenvic" "https://github.com/richimenvic/pyMenvic.extension.git"
-pyrevit reload
-echo Instalación completa.
-pause
