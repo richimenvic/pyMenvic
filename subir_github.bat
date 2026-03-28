39e5e9cb3fc3a5d40d8a2c298e92bb780c3fd7b7
Ricardo J. Mendieta <93284693+richimenvic@users.noreply.github.com>
2026-03-28 14:40:07 +0100

    actualizacion

--- subir_github.bat
+++ subir_github.bat
@@ -1,8 +1,10 @@
 @echo off
 cd /d "%~dp0"
 
+set /p MSG=Escribe el mensaje del commit: 
+
 git add .
-git commit -m "update"
+git commit -m "%MSG%"
 git pull --rebase origin main
 git push origin main
 
