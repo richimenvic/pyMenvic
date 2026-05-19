10f86fd3b0d7ce910a2823e4044510c55d57c94f
Menvic <93284693+richimenvic@users.noreply.github.com>
2026-05-19 12:57:26 +0200

    Use native pyRevit tab sorting pulse

--- pyMenvic.tab/Views.panel/TabManager.pushbutton/script.py
+++ pyMenvic.tab/Views.panel/TabManager.pushbutton/script.py
@@ -3,31 +3,75 @@
 __title__ = "Sort Tabs"
 __author__ = "Ricardo J. Mendieta"
 
-import os
-import sys
+from pyrevit.revit import tabs
+from pyrevit.userconfig import user_config
 
-try:
-    from lib.core.tab_sorter import sort_tabs_by_document
-except ImportError:
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    while True:
-        if os.path.basename(current_dir).lower() == "pymenvic.extension":
-            lib_dir = os.path.join(current_dir, "lib")
-            if lib_dir not in sys.path:
-                sys.path.append(lib_dir)
-            break
-        parent_dir = os.path.dirname(current_dir)
-        if parent_dir == current_dir:
-            break
-        current_dir = parent_dir
-    from core.tab_sorter import sort_tabs_by_document
+
+def _save_config():
+    try:
+        user_config.save_changes()
+    except:
+        pass
+
+
+def _get_theme():
+    try:
+        return tabs.get_tabcoloring_theme(user_config)
+    except:
+        return None
+
+
+def _set_sort_doc_tabs(theme, state):
+    try:
+        if theme and hasattr(theme, "SortDocTabs"):
+            theme.SortDocTabs = bool(state)
+            tabs.set_tabcoloring_theme(user_config, theme)
+            return True
+    except:
+        pass
+    return False
+
+
+def _native_sort_tabs_once():
+    theme = _get_theme()
+    previous_colorize = False
+    previous_sort = False
+
+    try:
+        previous_colorize = bool(getattr(user_config, "colorize_docs", False))
+    except:
+        previous_colorize = False
+
+    try:
+        if theme and hasattr(theme, "SortDocTabs"):
+            previous_sort = bool(theme.SortDocTabs)
+    except:
+        previous_sort = False
+
+    try:
+        _set_sort_doc_tabs(theme, True)
+        user_config.colorize_docs = True
+        _save_config()
+        tabs.init_doc_colorizer(user_config)
+    except:
+        pass
+
+    try:
+        if not previous_colorize:
+            user_config.colorize_docs = False
+            _save_config()
+            tabs.init_doc_colorizer(user_config)
+        else:
+            if theme:
+                _set_sort_doc_tabs(theme, previous_sort)
+                _save_config()
+                tabs.init_doc_colorizer(user_config)
+    except:
+        pass
 
 
 def main():
-    try:
-        sort_tabs_by_document()
-    except:
-        pass
+    _native_sort_tabs_once()
 
 
 if __name__ == "__main__":
