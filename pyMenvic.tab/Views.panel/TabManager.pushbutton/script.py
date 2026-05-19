63794e4ca7e598f6cc390319253a671113c11e18
Menvic <93284693+richimenvic@users.noreply.github.com>
2026-05-19 13:37:16 +0200

    Restore Sort Tabs as direct manual command

--- pyMenvic.tab/Views.panel/TabManager.pushbutton/script.py
+++ pyMenvic.tab/Views.panel/TabManager.pushbutton/script.py
@@ -4,20 +4,28 @@ __title__ = "Sort Tabs"
 __author__ = "Ricardo J. Mendieta"
 
 import os
+import sys
 
-STATE_FILE = os.path.join(os.environ.get("LOCALAPPDATA", os.environ.get("TEMP", os.getcwd())), "Temp", "pyMenvic", "tab_sort_once.txt")
-if not os.environ.get("LOCALAPPDATA", ""):
-    STATE_FILE = os.path.join(os.environ.get("TEMP", os.getcwd()), "pyMenvic", "tab_sort_once.txt")
+try:
+    from lib.core.tab_sorter import sort_tabs_by_document
+except ImportError:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    while True:
+        if os.path.basename(current_dir).lower() == "pymenvic.extension":
+            lib_dir = os.path.join(current_dir, "lib")
+            if lib_dir not in sys.path:
+                sys.path.append(lib_dir)
+            break
+        parent_dir = os.path.dirname(current_dir)
+        if parent_dir == current_dir:
+            break
+        current_dir = parent_dir
+    from core.tab_sorter import sort_tabs_by_document
 
 
 def main():
     try:
-        folder = os.path.dirname(STATE_FILE)
-        if folder and not os.path.exists(folder):
-            os.makedirs(folder)
-        f = open(STATE_FILE, "w")
-        f.write("1")
-        f.close()
+        sort_tabs_by_document()
     except:
         pass
 
