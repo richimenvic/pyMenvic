88d70504f8c5e7921dae695992bfb8234aa06e81
Menvic <93284693+richimenvic@users.noreply.github.com>
2026-05-19 13:01:11 +0200

    Stop touching pyRevit tab coloring from manual sorter

--- pyMenvic.tab/Views.panel/TabManager.pushbutton/script.py
+++ pyMenvic.tab/Views.panel/TabManager.pushbutton/script.py
@@ -3,51 +3,31 @@
 __title__ = "Sort Tabs"
 __author__ = "Ricardo J. Mendieta"
 
-from pyrevit.revit import tabs
-from pyrevit.userconfig import user_config
+import os
+import sys
 
-
-def _save_config():
-    try:
-        user_config.save_changes()
-    except:
-        pass
-
-
-def _get_theme():
-    try:
-        return tabs.get_tabcoloring_theme(user_config)
-    except:
-        return None
-
-
-def _set_sort_doc_tabs(theme, state):
-    try:
-        if theme and hasattr(theme, "SortDocTabs"):
-            theme.SortDocTabs = bool(state)
-            tabs.set_tabcoloring_theme(user_config, theme)
-            return True
-    except:
-        pass
-    return False
-
-
-def _sort_with_existing_tab_coloring():
-    theme = _get_theme()
-
-    # Do not disable pyRevit Tab Coloring. The user may already be using it.
-    # This button only asks pyRevit to sort/group document tabs using its native system.
-    try:
-        _set_sort_doc_tabs(theme, True)
-        user_config.colorize_docs = True
-        _save_config()
-        tabs.init_doc_colorizer(user_config)
-    except:
-        pass
+try:
+    from lib.core.tab_sorter import sort_tabs_by_document
+except ImportError:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    while True:
+        if os.path.basename(current_dir).lower() == "pymenvic.extension":
+            lib_dir = os.path.join(current_dir, "lib")
+            if lib_dir not in sys.path:
+                sys.path.append(lib_dir)
+            break
+        parent_dir = os.path.dirname(current_dir)
+        if parent_dir == current_dir:
+            break
+        current_dir = parent_dir
+    from core.tab_sorter import sort_tabs_by_document
 
 
 def main():
-    _sort_with_existing_tab_coloring()
+    try:
+        sort_tabs_by_document()
+    except:
+        pass
 
 
 if __name__ == "__main__":
