824b74b5f4ca1b382281b0b6cc69ffd1410a74c3
Menvic <93284693+richimenvic@users.noreply.github.com>
2026-05-19 11:35:25 +0200

    Use shared stable tab state in idling hook

--- hooks/idling.py
+++ hooks/idling.py
@@ -9,6 +9,7 @@ from pyrevit.userconfig import user_config
 
 try:
     from lib.core.tab_sorter import sort_tabs_by_document
+    from lib.core import tab_state
 except ImportError:
     current_dir = os.path.dirname(os.path.abspath(__file__))
     while True:
@@ -22,21 +23,12 @@ except ImportError:
             break
         current_dir = parent_dir
     from core.tab_sorter import sort_tabs_by_document
+    from core import tab_state
 
 
 PENDING_ENVVAR = "PYMENVIC_TABS_SORT_PENDING"
 LAST_RUN_ENVVAR = "PYMENVIC_TABS_SORT_LAST_RUN"
-PYMENVIC_SORT_ENVVAR = "PYMENVIC_TABS_BY_DOCUMENT_ENABLED"
-PYMENVIC_SORT_CONFIG = "pymenvic_sort_doc_tabs"
 MIN_INTERVAL_SECONDS = 0.50
-STATE_FILE = os.path.join(os.environ.get("TEMP", os.getcwd()), "pyMenvic_tab_sort_state.txt")
-
-
-def _safe_bool(value):
-    try:
-        return bool(value)
-    except:
-        return False
 
 
 def _safe_int(value, default_value):
@@ -53,68 +45,11 @@ def _safe_float(value, default_value):
         return default_value
 
 
-def _read_state():
-    data = {}
-    try:
-        if os.path.exists(STATE_FILE):
-            with open(STATE_FILE, "r") as state_file:
-                for line in state_file:
-                    if "=" in line:
-                        key, value = line.rstrip("\n").split("=", 1)
-                        data[key] = value
-    except:
-        pass
-    return data
-
-
-def _write_state(data):
-    try:
-        folder = os.path.dirname(STATE_FILE)
-        if folder and not os.path.exists(folder):
-            os.makedirs(folder)
-        with open(STATE_FILE, "w") as state_file:
-            for key in sorted(data.keys()):
-                state_file.write("{0}={1}\n".format(key, data[key]))
-    except:
-        pass
-
-
-def _update_state(**kwargs):
-    data = _read_state()
-    for key, value in kwargs.items():
-        data[key] = str(value)
-    _write_state(data)
-
-
-def _should_sort_tabs():
-    try:
-        if os.environ.get(PYMENVIC_SORT_ENVVAR, "") == "1":
-            return True
-    except:
-        pass
-
-    if _safe_bool(getattr(user_config, PYMENVIC_SORT_CONFIG, False)):
-        return True
-
-    if not _safe_bool(getattr(user_config, "colorize_docs", False)):
-        return False
-
-    try:
-        theme = tabs.get_tabcoloring_theme(user_config)
-        if hasattr(theme, "SortDocTabs"):
-            return _safe_bool(theme.SortDocTabs)
-    except:
-        pass
-
-    return False
-
-
 def _run_sort_if_due():
-    state = _read_state()
+    state = tab_state.read_state()
     hit_count = _safe_int(state.get("IDLING_HIT", "0"), 0) + 1
-    should_sort = _should_sort_tabs()
-    _update_state(
-        STATE_FILE=STATE_FILE,
+    should_sort = tab_state.is_enabled(user_config, tabs)
+    tab_state.update_state(
         IDLING_HIT=hit_count,
         IDLING_SHOULD_SORT="1" if should_sort else "0",
         IDLING_ERROR="",
@@ -122,7 +57,7 @@ def _run_sort_if_due():
 
     if not should_sort:
         os.environ[PENDING_ENVVAR] = "0"
-        _update_state(IDLING_MOVES="skipped")
+        tab_state.update_state(IDLING_MOVES="skipped")
         return
 
     now = time.time()
@@ -132,10 +67,9 @@ def _run_sort_if_due():
 
     try:
         moves = sort_tabs_by_document()
-        _update_state(IDLING_MOVES=moves, IDLING_LAST_RUN=now)
+        tab_state.update_state(IDLING_MOVES=moves, IDLING_LAST_RUN=now)
     except Exception as ex:
-        _update_state(IDLING_ERROR=str(ex).split("\n")[0])
-        moves = 0
+        tab_state.update_state(IDLING_ERROR=str(ex).split("\n")[0])
 
     os.environ[LAST_RUN_ENVVAR] = str(now)
 
@@ -150,4 +84,4 @@ def _run_sort_if_due():
 try:
     _run_sort_if_due()
 except Exception as ex:
-    _update_state(IDLING_ERROR=str(ex).split("\n")[0])
+    tab_state.update_state(IDLING_ERROR=str(ex).split("\n")[0])
